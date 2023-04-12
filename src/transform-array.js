d5ff9ba6d5618449a985fb1fa73ddd60dfd100c6
Ginezis <Ginezis25@gmail.com>
2023-04-13 02:07:01 +0300

    add solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+	if (!(arr instanceof Array)) {
+		throw Error(`'arr' parameter must be an instance of the Array!`);
+	}
+	const copyArr = arr.slice()
+	const result = []
+	for (let i = 0; i < copyArr.length; i++) {
+		if (copyArr[i] === '--discard-next') {
+			console.log(`нашел команду убрать некст`)
+			i++
+			continue
+		};
+		if (copyArr[i] === '--discard-prev') {
+			if (copyArr[i - 2] === '--discard-next' || i < 1) {
+				console.log(`Будет ошибка`)
+				continue
+			}
+
+			console.log(`нашел команду убрать прев`)
+			result.pop()
+			continue
+		};
+		if (copyArr[i] === '--double-next') {
+			if (i > copyArr.length - 2) {
+				console.log(`Будет ошибка`)
+				continue
+			}
+			console.log(`нашел команду повторить некст`)
+			result.push(copyArr[i + 1])
+			continue
+		};
+		if (copyArr[i] === '--double-prev') {
+			if (copyArr[i - 2] === '--discard-next' || i < 1) {
+				console.log(`Будет ошибка`)
+				continue
+			}
+			console.log(`нашел команду повторить прев`)
+			result.push(copyArr[i - 1])
+			continue
+		};
+		// if (typeof copyArr[i] === 'undefined') {
+		// 	console.log(`нашел дырку`)
+		// 	continue
+		// };
+		result.push(copyArr[i])
+	}
+
+
+	console.log('начало')
+	console.log(copyArr)
+	console.log('результат')
+	console.log(result)
+	return result
 }
 
 module.exports = {
-  transform
+	transform
 };
